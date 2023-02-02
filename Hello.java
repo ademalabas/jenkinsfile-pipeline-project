b25e943a151cb2c08cb7b6699a0b1e1b4a5c8404
ademalabas <ademalabas@gmail.com>
2023-02-02 19:47:24 +0300

    02.02.23

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Java TO THE WORLD");
+        System.out.println("Hello from Java TO THE WORLD...");
     }
 }
